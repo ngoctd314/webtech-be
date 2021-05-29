4b0381630e7f22408e15213b3740e6ad079d753d
tranngoc-git <tranngoc.contact@gmail.com>
2021-05-29 20:45:02 +0700

    add bash script for mocha

---
+++ mocha.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Declare variable
+type="full"
+path=""
+file=""
+
+for item in $1 $2 $3
+do
+  # Set comma as delimiter
+  IFS='='
+  #Read the split words into an array based on comma delimiter
+  read -a arr <<< "$item"
+  if [[ ${arr[0]} == "type" ]]
+  then
+    type=${arr[1]}
+  elif [[ ${arr[0]} == "path" ]]
+  then
+    path=${arr[1]}
+  elif [[ ${arr[0]} == "file" ]]
+  then
+    file=${arr[1]}
+  fi
+done
+
+if [[ $type == "full" ]]
+then
+  echo "Run Full Test"
+elif [[ $type == "folder" ]]
+then 
+  echo  "Run Folder Test On Path: ${path}"
+elif [[ $type == "file" ]]
+then 
+  echo "Run File Test on Path: ${path}/**/${file}.spec.ts"
+fi
+
+if [[ $type == "full" ]]
+then
+  NODE_ENV=test mocha --timeout 10000 -r ts-node/register -r tsconfig-paths/register --file src/__test__/setup.spec.ts src/**/__test__/**/*.spec.ts
+elif [[ $type == 'folder' ]]
+then
+  NODE_ENV=test mocha --timeout 10000 -r ts-node/register -r tsconfig-paths/register --file src/__test__/setup.spec.ts src/$path/__test__/**/*.spec.ts
+elif [[ $type == 'file' ]]
+then
+  NODE_ENV=test mocha --timeout 10000 -r ts-node/register -r tsconfig-paths/register --file src/__test__/setup.spec.ts src/$path/__test__/**/$file.spec.ts
+fi
