5ca7fa1f3f07abd626252fe81fa7d57b5b93193e
tranngoc-git <tranngoc.contact@gmail.com>
2021-05-29 20:46:48 +0700

    add database errors.hander

---
+++ src/common/index.ts
@@ -0,0 +1,5 @@
+import handleError from './errors.handle';
+import { connect as connectDB } from './database';
+
+// eslint-disable-next-line import/prefer-default-export
+export { handleError, connectDB };
