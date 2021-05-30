5d2c61b67a44dc6412fa16169b5ddb1cad3f3694
tranngoc-git <tranngoc.contact@gmail.com>
2021-05-30 17:13:10 +0700

    add client domain

--- src/resources/helper/jsonwebtoken.ts
+++ src/resources/helper/jsonwebtoken.ts
@@ -49,6 +49,7 @@ export const setJwtRes = (res: Response, token: string): Response => {
   return res.cookie('jwt', token, {
     maxAge: 30 * 86400 * 1000,
     httpOnly: true,
+    domain: 'https://web-tech-2021.web.app',
   });
 };
 
