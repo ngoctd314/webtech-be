0c25ee5782a93834f593ac535220844fc6c11291
tranngoc-git <tranngoc.contact@gmail.com>
2021-05-30 16:12:42 +0700

    remove dist

--- dist/src/errors/HttpException.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class HttpException extends Error {
-    constructor() {
-        super();
-        Object.setPrototypeOf(this, HttpException.prototype);
-    }
-}
-exports.default = HttpException;
--- dist/src/resources/comment/routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const controllers_1 = require("./controllers");
-const _middleware_1 = require("@middleware");
-const router = express_1.Router();
-router.post('/', _middleware_1.authenticated, controllers_1.crudComment._create);
-router.get('/', _middleware_1.authenticated, controllers_1.crudComment._read);
-exports.default = router;
